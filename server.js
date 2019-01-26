010f40f8de54b0e960a9f25685b034f4635f6d13
Eric Marquez <kenkm963@gmail.com>
2019-01-25 19:02:06 -0800

    Simple post server test

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+app.use(bodyParser.json({ type: 'application/*+json' }))
+app.use(bodyParser.text({ type: 'text/html' }));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+    console.log('From client' + req.body);
+    res.send('hello world');
+});
+
+app.get('/pizza', (req, res) => {
+    console.log(req.body.Title);
+    res.send('Your message was: ' + req.body.Value);
+});
+
+app.post('/ar', (req, res) => {
+    console.log(req.body.Title);
+    res.send('Post Request recieved: ' + req.body.Value);
+});
+
+// PORT
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
