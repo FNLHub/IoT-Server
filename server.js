9375fe4659ae04f82ed52b0d1d35ff7802cdcb7d
Eric Marquez <kenkm963@gmail.com>
2019-02-01 21:10:24 -0800

    Added index page

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 
+app.use(express.static(__dirname + '/views'));
 app.use(bodyParser.json({ type: 'application/*+json' }))
 app.use(bodyParser.text({ type: 'text/html' }));
 app.use(bodyParser.json());
