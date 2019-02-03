c7199e9311986089b4d35c4286a55fd5fc7c5bf1
Eric Marquez <kenkm963@gmail.com>
2019-02-02 16:26:59 -0800

    Updated project to working status
    - Changed server properties to be correct
    - Added AHub post request to handle Arduino connections
    - Added html file to display on index

--- server.js
+++ server.js
@@ -7,19 +7,13 @@ app.use(bodyParser.json({ type: 'application/*+json' }))
 app.use(bodyParser.text({ type: 'text/html' }));
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    console.log('From client' + req.body);
-    res.send('hello world');
-});
-
-app.get('/pizza', (req, res) => {
-    console.log(req.body.Title);
-    res.send('Your message was: ' + req.body.Value);
-});
-
-app.post('/ar', (req, res) => {
-    console.log(req.body.Title);
-    res.send('Post Request recieved: ' + req.body.Value);
+app.post('/AHub', (req, res) => {
+    if (req.body.SecretKey == 284923401){
+        res.send('!Hello from the server');
+    }
+    else{
+        res.send('Access denied');
+    }
 });
 
 // PORT
