ae558a9718ec91ebce42a86453619478967e2dce
Eric Marquez <kenkm963@gmail.com>
2019-02-22 19:08:08 -0800

    Update server.js

--- server.js
+++ server.js
@@ -9,18 +9,12 @@ app.use(bodyParser.text({ type: 'text/html' }));
 app.use(bodyParser.json());
 
 app.get('/Login', (req, res) => {
-    var result = 'Access Denied';
-
-    if (req.body.SecretKey == 284923401){
-        result = fanData;
-    }
-
     // should be a key that the server generates to establish a secure connection
     res.send('1234');
 });
 
 app.get('/FanSpeed', (req,res) => {
-    res.send(polled);
+    res.send('' + polled);
 });
 
 app.get('/Ping', (req, res) => {
